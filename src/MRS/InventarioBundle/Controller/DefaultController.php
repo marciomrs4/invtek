e96723045f8424acdfca6697c8c2762cbdb03cd6
Márcio Ramos <marciomrs4@hotmail.com>
2016-08-01 11:43:16 -0300

    Fixtures in security access

--- src/MRS/InventarioBundle/Controller/DefaultController.php
+++ src/MRS/InventarioBundle/Controller/DefaultController.php
@@ -10,10 +10,20 @@ class DefaultController extends Controller
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/",name="home")
+     * @Route("/",name="index")
      *
      */
     public function indexAction()
+    {
+        return $this->redirectToRoute('home');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/home",name="home")
+     *
+     */
+    public function homeAction()
     {
         return $this->render('MRSInventarioBundle:Default:index.html.twig');
     }
