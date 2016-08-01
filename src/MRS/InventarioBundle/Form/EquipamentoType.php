16d7744d1f8fa81c3a1e16fdc56436033658222a
Márcio Ramos <marciomrs4@hotmail.com>
2016-08-01 11:14:47 -0300

    Alter EquipamentoForm

--- src/MRS/InventarioBundle/Form/EquipamentoType.php
+++ src/MRS/InventarioBundle/Form/EquipamentoType.php
@@ -18,33 +18,33 @@ class EquipamentoType extends AbstractType
     {
         $builder
             ->add('nome',null,array('label'=>'nome',
-                                           'attr'=>array('class'=>'input-sm')))
+                'attr'=>array('class'=>'input-sm')))
+            ->add('centroMovimentacao',null,array('label'=>'centroMovimentacao',
+                'attr'=>array('class'=>'input-sm')))
+            ->add('fornecedor',null,array('label'=>'fornecedor',
+                'attr'=>array('class'=>'input-sm')))
+            ->add('marca',null,array('label'=>'marca',
+                'attr'=>array('class'=>'input-sm')))
+            ->add('tipoequipamento',null,array('label'=>'tipoequipamento',
+                'attr'=>array('class'=>'input-sm')))
             ->add('validade', DateType::class,array('label'=>'Vigência de Garantia',
-                                                    'widget'=>'single_text',
-                                                    'attr' => array('class'=>'input-sm')))
+                'widget'=>'single_text',
+                'attr' => array('class'=>'input-sm')))
             ->add('dataCompra',DateType::class,array('label'=>'Data da Compra',
-                                                     'widget'=>'single_text',
-                                                     'attr' => array('class'=>'input-sm')))
+                'widget'=>'single_text',
+                'attr' => array('class'=>'input-sm')))
             ->add('numeroserie',TextType::class,array('label'=>'numeroserie',
-                                           'attr'=>array('class'=>'input-sm')))
+                'attr'=>array('class'=>'input-sm')))
             ->add('status',null,array('label'=>'Status'))
             ->add('patrimonio',null,array('label'=>'patrimonio',
-                                           'attr'=>array('class'=>'input-sm')))
+                'attr'=>array('class'=>'input-sm')))
             ->add('descricao',null,array('label'=>'descricao',
-                                           'attr'=>array('class'=>'input-sm')))
+                'attr'=>array('class'=>'input-sm')))
             ->add('observacao',null,array('label'=>'observacao',
-                                           'attr'=>array('class'=>'input-sm')))
-            ->add('centroMovimentacao',null,array('label'=>'centroMovimentacao',
-                                           'attr'=>array('class'=>'input-sm')))
-            ->add('marca',null,array('label'=>'marca',
-                                           'attr'=>array('class'=>'input-sm')))
-            ->add('fornecedor',null,array('label'=>'fornecedor',
-                                           'attr'=>array('class'=>'input-sm')))
-            ->add('tipoequipamento',null,array('label'=>'tipoequipamento',
-                                           'attr'=>array('class'=>'input-sm')))
+                'attr'=>array('class'=>'input-sm')))
         ;
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
